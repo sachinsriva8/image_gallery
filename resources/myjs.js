873e91444c94725939ca7bd1a7eb94eeddc0c881
sachin <sachin.sriva8@gmail.com>
2023-07-19 00:38:31 +0530

    added window event as well as made code clean

--- resources/myjs.js
+++ resources/myjs.js
@@ -5,34 +5,43 @@ let modal=document.getElementById("modal");
 let modal_image=document.getElementById("modal-content");
 let close_button=document.getElementById("close");
 
-document.createElement
 async function image_data(){
     let response= await fetch("https://jsonplaceholder.typicode.com/albums/1/photos");
      images_data= await response.json();
-    // console.log(images_data);
-    for(let i=0; i<20;i++){
-        // let image_url=images_data[i].url;
-        console.log(images_data[i].url);
+    console.log(images_data);
+    for(let i=0; i<images_data.length;i++){
+        // console.log(images_data[i].url);
         let img=document.createElement("img");
         img.className="thumbnail";
         img.src=`${images_data[i].url}`;
         gallery.append(img);
         // console.log(img);
 
+        // adding click event on each image to to kow with image is clicked.
         img.addEventListener("click",(e)=>{
             var img_url=e.target.src;
             open_modal(img_url);
         })
     }
+    //function to open modal
     function open_modal(urls){
         modal.style.display="block"
         modal_image.src=urls;
     }
+    //function to close modal
     close_button.addEventListener("click", (e)=>{
         modal.style.display="none";
     })
+    //function to close modal when user clicks outside image.
+    window.addEventListener("click", (e)=>{
+        if (e.target==modal){
+            // console.log(e.target);
+            modal.style.display="none";
+        }
+    })
+
 }
 
 image_data();
-// console.log(images_data);
+
 
